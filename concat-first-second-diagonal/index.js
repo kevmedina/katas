e5d6e428f24f8fd6d0aae2080ab46f2b01413edd
Kevin Medina <kmedinakm@hotmail.com>
2020-01-17 22:32:18 -0500

    iteration 2 complete - all done

--- concat-first-second-diagonal/index.js
+++ concat-first-second-diagonal/index.js
@@ -38,7 +38,14 @@ From a two dimensional array (with the same height and width), extract and conca
 
 function concatSecondDiagonal(twoDimArray) {
     // your code goes here
-    
+    let result = '';
+    for (let r = 0; r < twoDimArray.length; r++) {
+      for (let c = twoDimArray[r].length - 1; c >= 0; c--) {
+        if (r === c)
+          result += twoDimArray[r][twoDimArray.length - (r + 1)];
+      }
+    }
+    return result;
 }
 
 // If given the following array, here is the result you should expect:
@@ -55,4 +62,3 @@ const someArr2 = [
 // solution:
 console.log(concatSecondDiagonal(someArr2));
 // ==> "Berlin"
-
