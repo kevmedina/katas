079a03b51a66ea61a8c059760737fc8d1cb29d25
Kevin Medina <kmedinakm@hotmail.com>
2020-01-20 20:58:48 -0500

    done

--- rgb-to-hex-conversion/index.js
+++ rgb-to-hex-conversion/index.js
@@ -8,3 +8,18 @@ rgb(255, 255, 300) // returns FFFFFF
 rgb(0,0,0) // returns 000000
 rgb(148, 0, 211) // returns 9400D3
 */
+
+function rgb(r, g, b){
+    return toHex(r) + toHex(g) + toHex(b);
+  }
+  
+  function toHex(d) {
+      if(d < 0 ) {return "00";}
+      if(d > 255 ) {return "FF";}
+      return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()  //I like this padding part.
+  }
+
+console.log(rgb(255, 255, 255)) // returns FFFFFF
+console.log(rgb(255, 255, 300)) // returns FFFFFF
+console.log(rgb(0,0,0)) // returns 000000
+console.log(rgb(148, 0, 211)) // returns 9400D3
