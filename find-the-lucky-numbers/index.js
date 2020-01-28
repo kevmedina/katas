5cc43ffd4c143f593fda3c51281b5f99042b25f8
Kevin Medina <kmedinakm@hotmail.com>
2020-01-27 20:27:33 -0500

    add two solutions

--- find-the-lucky-numbers/index.js
+++ find-the-lucky-numbers/index.js
@@ -1,5 +1,11 @@
 // Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
 
 function filterLucky(arr) {
+    // return arr.filter(num => num.toString().includes(7));
 
+    // Solution 2
+    return arr.filter((num) => (num.toString().match(/7/) != null));
 }
+
+console.log(filterLucky([1,2,3,4,5,6,7,68,69,70,15,17]));
+console.log(filterLucky([17,30,47,50,62,77,117]));
