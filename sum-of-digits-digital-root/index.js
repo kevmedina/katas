1a7ca5ab20c64acc9e005c816049a04336a58fac
Kevin Medina <kmedinakm@hotmail.com>
2020-01-31 22:09:30 -0500

    add first solution

--- sum-of-digits-digital-root/index.js
+++ sum-of-digits-digital-root/index.js
@@ -4,12 +4,24 @@
 // output: single digit number 
 
 function digital_root(n) {
-    
+    var sumOfNums = 0;
+    var arrOfNums = n.toString().split("").map(function(str) {  // [ 9, 4, 2 ]
+      return parseInt(str); 
+    });
+    while (arrOfNums.length > 1) {
+      sumOfNums = arrOfNums.reduce(function(a, b) { // 15
+        return a + b;
+      });
+      arrOfNums = sumOfNums.toString().split("").map(function(str) { // turns 15 into [1, 5]
+      return parseInt(str);
+      });
+    }
+    return sumOfNums;
   }
   
-  //digital_root(1); // 0
-  //digital_root(16); // 1 + 6 = 7
-  //digital_root(942); // 9 + 4 + 2 = 15, 1 + 5 = 6
-  //digital_root(132189); // 1 + 3 + 2 + 1 + 8 + 9 = 24, 2 + 4 = 6
-  //digital_root(493193); // 4 + 9 + 3 + 1 + 9 + 3 = 29, 2 + 9 = 11, 1 + 1 = 2
+  console.log(digital_root(1)); // 0
+  console.log(digital_root(16)); // 1 + 6 = 7
+  console.log(digital_root(942)); // 9 + 4 + 2 = 15, 1 + 5 = 6
+  console.log(digital_root(132189)); // 1 + 3 + 2 + 1 + 8 + 9 = 24, 2 + 4 = 6
+  console.log(digital_root(493193)); // 4 + 9 + 3 + 1 + 9 + 3 = 29, 2 + 9 = 11, 1 + 1 = 2
   
