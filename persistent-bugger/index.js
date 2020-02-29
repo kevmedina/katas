5431b09bd42e170ef24ff8ed0e43bb27d881a633
Kevin Medina <kmedinakm@hotmail.com>
2020-02-29 11:45:31 -0500

    add first solution

--- persistent-bugger/index.js
+++ persistent-bugger/index.js
@@ -11,7 +11,18 @@
 //  persistence(4) === 0 // because 4 is already a one-digit number
 
 function persistence(num) {
-   
+    // Solution 1
+    let count = 0;
+    while (num > 9) {
+        let str = num.toString().split('');
+        let total = str.reduce((acc, currentValue) => {
+            return acc * currentValue;
+        },1);
+        console.log(total);
+         num = total;
+        count++;
+    }
+    return 'Total count: ' + count;
 }
 
 console.log(persistence(39));
