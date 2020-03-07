0e5495f5e9b5383bcc9d75611fe592185577ca2d
Kevin Medina <kmedinakm@hotmail.com>
2020-03-07 11:18:12 -0500

    add solution two

--- backspaces-in-string/index.js
+++ backspaces-in-string/index.js
@@ -9,23 +9,36 @@
 // ""              ==>  ""
 
 // Solution 1
-function cleanString(str) {
-  var strArr = str.split("");
-  if (strArr === [] || strArr.indexOf("#") === -1) {
-    return strArr.join("");
-  } else {
-    for (var i = 0; i < strArr.length; i++) {
-      if (strArr[i] === "#") {
-        if (strArr[i - 1] === undefined) {
-          strArr.splice(i, 1);
-          return cleanString(strArr.join(""));
-        } else {
-          strArr.splice(i - 1, 2);
-          return cleanString(strArr.join(""));
-        }
+// function cleanString(str) {
+//   var strArr = str.split("");
+//   if (strArr === [] || strArr.indexOf("#") === -1) {
+//     return strArr.join("");
+//   } else {
+//     for (var i = 0; i < strArr.length; i++) {
+//       if (strArr[i] === "#") {
+//         if (strArr[i - 1] === undefined) {
+//           strArr.splice(i, 1);
+//           return cleanString(strArr.join(""));
+//         } else {
+//           strArr.splice(i - 1, 2);
+//           return cleanString(strArr.join(""));
+//         }
+//       }
+//     }
+//   }
+// }
+
+// Solution 2
+function cleanString(s) {
+    const result = []
+    for (const c of s) {
+      if (c === "#") {
+        result.pop()
+      } else {
+        result.push(c)
       }
     }
+    return result.join("")
   }
-}
 
 console.log(cleanString("abc#d##c"));
