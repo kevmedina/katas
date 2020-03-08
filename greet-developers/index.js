ea3c5ad8a334ca6dd015593d387a6892dbd56ddf
Kevin Medina <kmedinakm@hotmail.com>
2020-03-08 19:53:39 -0400

    add third solution

--- greet-developers/index.js
+++ greet-developers/index.js
@@ -68,4 +68,9 @@ function greetDevelopers(list) {
   return list;
 }
 
+// Soultion 3
+const greetDevelopers = list => list.map(
+    dev => ({...dev, greeting: `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`})
+  );
+
 console.log(greetDevelopers(list1));
