0bf985f361425f9642cac860f3b67fb0e17db186
Kevin Medina <kmedinakm@hotmail.com>
2020-03-26 22:00:28 -0400

    add second solution

--- buying-a-car/index.js
+++ buying-a-car/index.js
@@ -12,29 +12,56 @@
 
 // parameter (positive int or float, guaranteed) startPriceOld (Old car price)
 // parameter (positive int or float, guaranteed) startPriceNew (New car price)
-// parameter (positive int or float, guaranteed) savingperMonth 
+// parameter (positive int or float, guaranteed) savingperMonth
 // parameter (positive float or int, guaranteed) percentLossByMonth
 
 // nbMonths(2000, 8000, 1000, 1.5) should return [6, 766] or (6, 766)
 
-function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth) {
-    let months = 0;
-    let savingsTotal = 0;
-    let lossPercentage = percentLossByMonth;
-    let oldCarValue = startPriceOld;
-    let newCarValue = startPriceNew;
-    
-    while ((oldCarValue + savingsTotal) < newCarValue) {
-      months++;
-      months % 2 === 0 ? lossPercentage += (.5) : lossPercentage;
-      
-      oldCarValue -= oldCarValue * (lossPercentage / 100);
-      newCarValue -= newCarValue * (lossPercentage / 100);
-      savingsTotal += savingperMonth;
-    }
-    
-    let leftoverSavings = (oldCarValue + savingsTotal) - newCarValue;
-    return [months, Math.round(leftoverSavings)];
+// Solution 1
+function nbMonths(
+  startPriceOld,
+  startPriceNew,
+  savingperMonth,
+  percentLossByMonth
+) {
+  let months = 0;
+  let savingsTotal = 0;
+  let lossPercentage = percentLossByMonth;
+  let oldCarValue = startPriceOld;
+  let newCarValue = startPriceNew;
+
+  while (oldCarValue + savingsTotal < newCarValue) {
+    months++;
+    months % 2 === 0 ? (lossPercentage += 0.5) : lossPercentage;
+
+    oldCarValue -= oldCarValue * (lossPercentage / 100);
+    newCarValue -= newCarValue * (lossPercentage / 100);
+    savingsTotal += savingperMonth;
   }
 
-  console.log(nbMonths(2000, 8000, 1000, 1.5));
+  let leftoverSavings = oldCarValue + savingsTotal - newCarValue;
+  return [months, Math.round(leftoverSavings)];
+}
+
+// Solution 2
+function nbMonths(
+  startPriceOld,
+  startPriceNew,
+  savingperMonth,
+  percentLossByMonth
+) {
+  var months = 0,
+    moneySaved = 0;
+  while (startPriceNew > startPriceOld + moneySaved) {
+    moneySaved += savingperMonth;
+    startPriceOld -= startPriceOld * (percentLossByMonth / 100);
+    startPriceNew -= startPriceNew * (percentLossByMonth / 100);
+    months++;
+    if (months % 2 == 1) {
+      percentLossByMonth += 0.5;
+    }
+  }
+  return [months, Math.round(startPriceOld + moneySaved - startPriceNew)];
+}
+
+console.log(nbMonths(2000, 8000, 1000, 1.5));
